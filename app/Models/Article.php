d3bb924f8c754ffa0a43768f61b12210c4566fd6
Raul Bonifacio <raulbonifacio@localhost.localdomain>
2019-12-02 20:34:01 -0300

    - Models updated.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
-{
-    //
+class Article extends Model {
+    
+    protected $fillable = ['title', 'link'];
 }
