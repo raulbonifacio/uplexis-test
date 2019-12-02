16718a6db926cd9c1e60472a902cf4d1d1335532
Raul Bonifacio <raulbonifacio@localhost.localdomain>
2019-12-02 20:34:25 -0300

    - Controllers finished.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
+use \App\Models\Article;
 use \App\Http\Gateways\Contracts\ArticleGateway;
+use \App\Http\Gateways\Contracts\ArticleGatewayException;
+
 use Illuminate\Http\Request;
+
 
 class ArticleController extends Controller
 {
@@ -13,11 +18,56 @@ class ArticleController extends Controller
         $this->articleGateway = $articleGatewayProvided;
     }
 
-    public function index(ArticleGateway $articleGatewayProvided) {
+    public function index(Request $request) {
 
-        $articles = $this->articleGateway->fetchArticles();
+        //Return the information for the view
+        return view('pages.articles.index', [
+            'articles' => auth()->user()->articles()->orderBy('created_at', 'desc')->get(),
+        ]);
+    }
 
-        
-        return "something";
+    public function fetch(Request $request) {
+
+        $capturedArticles = [];
+        $duplicates = 0 ;
+
+        $request->validate([
+            'search' => 'required'
+        ]);
+ 
+        try { 
+            $capturedArticles = $this->articleGateway->fetchArticles($request->search);
+        } catch (ArticleGatewayException $exception) { 
+            redirect()->back()->withErrors(['Something wrong happened while fetching the articles']);
+        }
+
+        //Saves the articles
+        collect($capturedArticles)->each(function($article) use ( &$duplicates ) {
+
+            //Prevent duplicates in the database
+            if(!Article::where('title', $article->title)->count())
+            {
+                auth()->user()->articles()->save($article);
+            } else {
+                $duplicates++;
+            }
+        });
+
+        //Load the user articles
+        auth()->user()->load('articles');
+
+        //Return the information for the view
+        return view('pages.articles.index', [
+            'articles' => auth()->user()->articles()->orderBy('created_at', 'desc')->get(),
+            'capturedArticles' => $capturedArticles,
+            'duplicates' => $duplicates,
+            'search' => $request->search,
+        ]);
+    }
+
+    public function remove($id) { 
+        $article = Article::findOrFail($id);
+        $article->delete();
+        return response()->json($article, 200);
     }
 }
